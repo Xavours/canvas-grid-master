f698fd5011f42b9598ef995ea9190efe971ff30a
Xavours <contact@xavierorssaud.com>
2017-06-13 13:43:57 -0400

    reindent gruntfile

--- gruntfile.js
+++ gruntfile.js
@@ -1,70 +1,70 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-	  
-	//  Convert SASS files into CSS  
-  	sass: {
-        options: {
-            sourceMap: true
-        },
-        dist: {
-            files: {
-            	'css/style.css': 'css/style.scss'
-            }
-        }
-    },
+	// Project configuration.
+	grunt.initConfig({
 
-	//  Minify CSS  
-    cssmin: {
-	  target: {
-	    files: {
-	      'min/app.min.css': 'css/*.css'
-	    }
-	  }
-	},
+		//  Convert SASS files into CSS  
+		sass: {
+			options: {
+				sourceMap: true
+			},
+			dist: {
+				files: {
+					'css/style.css': 'css/style.scss'
+				}
+			}
+		},
 
-    //  Autoprefix CSS  
-	autoprefixer: {
-      styles: {
-        files: {
-          'min/app.min.css': 'min/app.min.css'
-        }
-      }
-    },
+		//  Minify CSS  
+		cssmin: {
+			target: {
+				files: {
+					'min/app.min.css': 'css/*.css'
+				}
+			}
+		},
+
+		//  Autoprefix CSS  
+		autoprefixer: {
+			styles: {
+				files: {
+					'min/app.min.css': 'min/app.min.css'
+				}
+			}
+		},
+		
+		//  Watch
+		watch: {
+			scripts: {
+				files: ['**/*.scss', '**/*.js', 'gruntfile.js'],
+				tasks: ['build'],
+				options: {
+					spawn: false,
+					livereload: 8888,
+				},
+			},
+		},
 	
-	//  Watch
-    watch: {
-      scripts: {
-        files: ['**/*.scss', '**/*.js', 'gruntfile.js'],
-        tasks: ['build'],
-        options: {
-          spawn: false,
-          livereload: 8888,
-        },
-      },
-    },
-	
-	//  Minify JS
-    uglify: {
-      options: {
-        mangle: false
-      },
-      my_target: {
-        files: {
-	  'min/canvas-grid-master.min.js': ['js/functions.js',
-               'js/main.js',
-	  					 'js/options.js',
-	  					 'js/trigger.js',
-	  					 'js/easing.js'],
-	  
-        }
-      }
-    },
-    
-	//  Clean .css and .css.map from sass files
-    clean: ["css/*.css.map"]
-  });
+		//  Minify JS
+		uglify: {
+			options: {
+				mangle: false
+			},
+		my_target: {
+			files: {
+				'min/canvas-grid-master.min.js': [
+					'js/functions.js',
+					'js/main.js',
+					'js/options.js',
+					'js/trigger.js',
+					'js/easing.js'],
+				}
+			}
+		},
+		
+		//  Clean .css and .css.map from sass files
+		clean: ["css/*.css.map"]
+	});
 
 
   // load grunt modules
