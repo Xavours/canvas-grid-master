182e65bb6fd0281d3fb1319a21094556b1bdd66a
Xavours <contact@xavierorssaud.com>
2017-06-16 22:36:49 -0400

    new folder structure

---
+++ src/options.js
@@ -0,0 +1,150 @@
+//  Valid Options
+function validOptions(param){
+	var valid = true;
+	
+	//  Check if param is an object
+	if (param === 'undefined') {
+		console.error('Settings object is undefined');
+		valid = false;
+		return valid;
+		
+	} else if (param === null) {
+		console.error('Settings object is null');
+		valid = false;
+		return valid;
+		
+	} else if (!param && Object.toType(param)) {
+		console.log('Settings object valid');
+	}
+	
+	//  Check if properties one by one
+
+		//console.log(param);
+		//console.log( window[param.libraryName] );
+		
+		//  Main options
+		if ( param.hasOwnProperty('tileWidth') ) {
+			if( Object.toType(param.tileWidth) !== 'number'){
+				console.error('The option "tileWidth" must be a number');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('tileHeight') ) {
+			if( Object.toType(param.tileHeight) !== 'number'){
+				console.error('The option "tileHeight" must be a number');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('content') ) {
+			if( Object.toType(param.content) !== 'string'){
+				console.error('The option "content" must be a string');
+				valid = false;
+			} else if ( param.content !== 'image' && param.content !== 'rectangle') {
+				console.error('The option "content" : ' + param.content + ' is not a valid');
+				valid = false;
+			}
+		}
+
+		if ( param.hasOwnProperty('grayScaleOn') ) {
+			if( Object.toType(param.scaleOn) !== 'boolean'){
+				console.error('scaleOn must be a boolean');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('fadeAnimation') ) {
+			if( Object.toType(param.fadeAnimation) !== 'string'){
+				console.error('The option "fadeAnimation" must be a string');
+				valid = false;
+			} else if ( param.fadeAnimation !== 'easeOutCubic' ) {
+				console.error('The option "fadeAnimation" : ' + param.fadeAnimation + ' is not a valid');
+				valid = false;
+			}
+		}
+
+		//  Controllers options
+		if ( param.hasOwnProperty('controller') ) {
+			if( Object.toType(param.controller) !== 'string'){
+				console.error('The option "controller" must be a string');
+				valid = false;
+			} else if ( param.controller !== 'mouse' && param.controller !== 'keyboard' && param.controller !== 'gamepad') {
+				console.error('The option "controller" : ' + param.controller + ' is not a valid');
+				valid = false;
+			}
+		}
+
+		if ( param.hasOwnProperty('moveStep') ) {
+			if( Object.toType(param.moveStep) !== 'number'){
+				console.error('The option "moveStep" must be a number');
+				valid = false;
+			}
+		}
+		
+		//  Scale Options
+		if ( param.hasOwnProperty('scaleOn') ) {
+			if( Object.toType(param.scaleOn) !== 'boolean'){
+				console.error('The option "scaleOn" must be a boolean');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('scale') ) {
+			if( Object.toType(param.scale) !== 'number'){
+				console.error('The option "scale" must be a number');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('minScale') ) {
+			if( Object.toType(param.minScale) !== 'number'){
+				console.error('The option "minScale" must be a number');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('maxScale') ) {
+			if( Object.toType(param.maxScale) !== 'number'){
+				console.error('The option "maxScale" must be a number');
+				valid = false;
+			}
+		}
+		
+		
+		//  Spread mode
+		if ( param.hasOwnProperty('spreadMode') ) {
+			if( Object.toType(param.spreadMode) !== 'string'){
+				console.error('The option "spreadMode" must be a string');
+				valid = false;
+			} else if ( param.spreadMode !== 'random' && param.spreadMode !== 'shuffle') {
+				console.error('The option "spreadMode" : ' + param.spreadMode + ' is not a valid');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('numberTile') ) {
+			if( Object.toType(param.numberTile) !== 'number'){
+				console.error('The option "numberTile" must be a number');
+				valid = false;
+			}
+		}
+		
+		
+		//  Methods
+		if ( param.hasOwnProperty('onClickCallback') ) {
+			if( Object.toType(param.onClickCallback) !== 'function'){
+				console.error('The option "onClickCallback" must be a function');
+				valid = false;
+			}
+		}
+		
+		if ( param.hasOwnProperty('mouseoverCallback') ) {
+			if( Object.toType(param.mouseoverCallback) !== 'function'){
+				console.error('The option ""mouseoverCallback" must be a function');
+				valid = false;
+			}
+		}
+	
+	return valid;
+}
---
+++ src/trigger.js
@@ -0,0 +1,317 @@
+// event handlers
+
+	var el = document.getElementById('viewport');
+	
+	// create a simple instance
+	// by default, it only adds horizontal recognizers
+	var eventHandler = new Hammer(el);
+	
+	// let the pan gesture support all directions.
+	// this will block the vertical scrolling on a touch-device while on the element
+	eventHandler.get('pan').set({ direction: Hammer.DIRECTION_ALL });
+	eventHandler.get('pinch').set({ enable: true });
+	eventHandler.get('rotate').set({ enable: true });
+	
+	// listen to events...
+	/*
+eventHandler.on("panleft panright panup pandown tap press pinch rotate", function(ev) {
+	    console.log(ev.type +" gesture detected.");
+	});
+*/
+
+/////////
+//  Mouse control functions
+/////////
+
+	//  Initialize variable
+		var lastX, lastY;
+		var deltaX = 0;
+		var deltaY = 0;
+		var intervalCounter = 0;
+		var startWheel, lastWheel, diffWheel;
+		startWheel = lastWheel = new Date();
+		var pinchSensitivity = 6 / 100000;
+		
+	function OpenInNewTab(url) {
+		var win = window.open(url, '_blank');
+		win.focus();
+	}
+	
+	//  Work around because of Firefox / IE / opera
+	function applyWheel(e){
+	    var evt = window.event || e 									//equalize event object
+	    var delta = evt.detail? evt.detail*(-120) : evt.wheelDelta 		//check for detail first so Opera uses that instead of wheelDelta
+	    
+	    //  If you start wheeling the mouse stock for the first time since 3 seconds, update viewportX, Y
+	    	startWheel = new Date();
+	    	diffWheel = (startWheel- lastWheel) / 1000;
+
+	    //console.log(delta);
+
+	    wall.goScale(delta);
+	}
+
+
+/////////
+//  Keyboard control functions
+/////////
+
+
+/////////
+//   Bind Gamepad control
+/////////
+
+	var tileCentered;
+	var lastTileCentered;
+
+	// Gamepad detection	
+	var gamepad = new Gamepad();
+
+	gamepad.on('connect', function (e) {
+		console.log('controller ' + e.index + ' connected!');
+	});
+
+	gamepad.on('disconnect', function (e) {
+		console.log('controller ' + e.index + ' disconnected!');
+	});
+
+
+/////////
+//  Set triggers methods
+/////////
+
+	Wall.prototype.setTrigger = function () {
+		var self = this;
+
+		//  Mouse controls are binded only if in the settings 'controller' is set on 'mouse'
+		if (this.settings.controller == 'mouse') {
+
+			//  Bind pan to scroll in any direction
+			eventHandler.on("panstart", function(e) {
+
+				lastX = e.center.x;
+				lastY = e.center.y;
+
+			});
+			
+			eventHandler.on("panmove", function(e) {
+
+				deltaX = lastX - e.center.x;
+				deltaY = lastY - e.center.y;
+
+				self.goTranslate(deltaX, deltaY);    
+
+				lastX = e.center.x;
+				lastY = e.center.y;
+
+			});
+			
+			eventHandler.on("panend", function(e) {
+
+		        //  When you finish paning, update viewportX, Y
+		        //  Wait 0.5 sec to update it though cause the movement might not be finished when you stop paning                 
+		        
+		    });
+			
+			//  Bind mousewheel to scale
+			//  FF doesn't recognize mousewheel as of FF3.x
+			var mousewheelevt=(/Firefox/i.test(navigator.userAgent))? "DOMMouseScroll" : "mousewheel" 
+
+			//  if IE (and Opera depending on user setting)
+			if (el.attachEvent)
+				el.attachEvent("on"+mousewheelevt, applyWheel)
+
+				//  WC3 browsers
+				else if (el.addEventListener)
+					el.addEventListener(mousewheelevt, applyWheel, false);
+
+			//  Bind click
+			eventHandler.on("tap", function(e) {
+
+				var target = self.getTile(e.center.x, e.center.y)
+				self.onClick(target);
+
+			});
+			
+			
+			//  Bind hover
+			el.addEventListener('mousemove', function(e) {
+
+				var target = self.getTile(e.pageX, e.pageY);
+				self.onMouseover(target);
+
+			});
+			
+			
+			//  Bind pinch
+			eventHandler.on("pinchin", function(e) {
+
+				self.goScale(e.scale);
+
+			});
+			
+			eventHandler.on("pinchout", function(e) {
+
+				self.goScale(-e.scale);
+
+			});
+		}	
+
+		//  Keyboard controls are binded only if in the settings 'controller' is set on 'keyboard'
+		if (this.settings.controller == 'keyboard') {
+			$(document).keydown(function (e) {
+				switch (e.which) {
+
+					case 37:
+				        // left
+				        self.goTranslate(-self.settings.moveStep, 0);
+				        break;
+
+				        case 38:
+				        // up
+				        self.goTranslate(0, self.settings.moveStep);
+				        break;
+
+				        case 39:
+				        // right
+				        self.goTranslate(self.settings.moveStep, 0);
+				        break;
+
+				        case 40:
+				        // down
+				        self.goTranslate(0, -self.settings.moveStep);
+				        break;
+
+				        case 79:
+				        // "o" -->>>  zoom in
+				        self.goScale(100);
+				        break;
+
+				        case 80:
+				        // "p" -->>>  zoom out
+				        self.goScale(-100);
+				        break;
+
+				        default:
+				        return; // exit this handler for other keys
+				    }
+				e.preventDefault(); // prevent the default action (scroll / move caret)
+			});
+		}
+
+		// Gamepad controls are binded only if in the settings 'controller' is set on 'gamepad'
+		if (this.settings.controller == 'gamepad') {
+
+			// Cross
+			gamepad.on('press', 'button_1', function () {
+				console.log('Button cross was pressed!');
+				self.settings.onPressButton1Callback(tileCentered);
+			});
+
+			// Circle
+			gamepad.on('press', 'button_2', function () {
+				console.log('Button circle was pressed!');
+				self.settings.onPressButton2Callback();
+			});
+
+			// Square
+			gamepad.on('press', 'button_3', function () {
+				console.log('Button square was pressed!');
+				self.settings.onPressButton3Callback();
+			});
+
+			// Triangle
+			gamepad.on('press', 'button_4', function () {
+				console.log('Button triangle was pressed!');
+				self.settings.onPressButton4Callback();
+			});
+
+			// Analog left
+			gamepad.on('hold', 'stick_axis_left', function (e) {
+				//console.log(e.button + ' was pressed!');
+				//  Update PositionX
+				self.goTranslate(e.value[0]*5, e.value[1]*5);
+
+				tileCentered = self.getTile(self.width/2, self.height/2);
+				tileCentered.focus = true;
+
+				if ( isEmpty(lastTileCentered) ) {
+					lastTileCentered = tileCentered;
+				} else if (tileCentered !== lastTileCentered) {
+					lastTileCentered.focus = false;
+					lastTileCentered = tileCentered;
+				}
+
+			});
+
+			// Analog right
+			gamepad.on('hold', 'stick_axis_right', function (e) {
+				//console.log(e.button + ' was pressed!');
+				self.goScale(-e.value[1]*10);
+			});
+
+			// D-Pad Up
+			gamepad.on('hold', 'd_pad_up', function (e) {
+				console.log(e.button + ' was pressed!');
+				self.settings.onPressDPadUpCallback();
+			});
+
+			// D-Pad Down
+			gamepad.on('hold', 'd_pad_down', function (e) {
+				console.log(e.button + ' was pressed!');
+				self.settings.onPressDPadDownCallback();
+			});
+
+			// D-Pad Left
+			gamepad.on('hold', 'd_pad_left', function (e) {
+				console.log(e.button + ' was pressed!');
+				self.settings.onPressDPadLeftCallback();
+			});
+
+			// D-Pad Right
+			gamepad.on('hold', 'd_pad_right', function (e) {
+				console.log(e.button + ' was pressed!');
+				self.settings.onPressDPadRightCallback();
+			});
+
+			// Start
+			gamepad.on('press', 'start', function (e) {
+				console.log(e.button + ' was pressed!');
+				self.settings.onPressStartCallback();
+			});
+
+			// Select
+			gamepad.on('press', 'select', function (e) {
+				console.log(e.button + ' was pressed!');
+				self.settings.onPressSelectCallback();
+			});
+		}
+	}
+	
+//  Bind Window resize
+window.onresize = function(event) {
+    wall.updateSizeViewport();
+};
+
+// Speed up calls to hasOwnProperty
+var hasOwnProperty = Object.prototype.hasOwnProperty;
+
+function isEmpty(obj) {
+
+    // null and undefined are "empty"
+    if (obj == null) return true;
+
+    // Assume if it has a length property with a non-zero value
+    // that that property is correct.
+    if (obj.length > 0)    return false;
+    if (obj.length === 0)  return true;
+
+    // Otherwise, does it have any properties of its own?
+    // Note that this doesn't handle
+    // toString and valueOf enumeration bugs in IE < 9
+    for (var key in obj) {
+        if (hasOwnProperty.call(obj, key)) return false;
+    }
+
+    return true;
+}
